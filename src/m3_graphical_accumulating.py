7a48e1e414ebf57e6ab7dd0bcb5438d7859af28c
watsonzr <watsonzr@rose-hulman.edu>
2018-03-12 11:22:00 -0400

    Final Version of m3

--- src/m3_graphical_accumulating.py
+++ src/m3_graphical_accumulating.py
@@ -9,8 +9,8 @@ Additionally, it emphasizes that you must
 before you can implement a solution to the problem in Python.
 
 Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Zack Watson.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -96,7 +96,7 @@ def draw_parallel_lines(n, point, length, window):
       :type window: rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
@@ -109,6 +109,13 @@ def draw_parallel_lines(n, point, length, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ####################################################################
     # ------------------------------------------------------------------
+
+    for _ in range(n):
+        point2 = rg.Point(point.x + length, point.y)
+        line = rg.Line(point, point2)
+        line.attach_to(window)
+        window.render(1)
+        point.y = point.y + 30
 
 
 def run_test_draw_lines():
@@ -161,7 +168,7 @@ def draw_lines(n, point, window):
       :type window: rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     #
     # CONSIDER using the ACCUMULATOR IN GRAPHICS pattern,
@@ -174,6 +181,18 @@ def draw_lines(n, point, window):
     #          ** FIRST DO A CONCRETE EXAMPLE BY HAND! **
     ####################################################################
     # ------------------------------------------------------------------
+
+    pointsx = point.x + 100
+    spacing = 200 / (n-1)
+    accum = 0
+
+    for _ in range(n):
+        point1 = rg.Point(point.x, point.y - 100 + accum)
+        point2 = rg.Point(pointsx, point1.y)
+        line = rg.Line(point1, point2)
+        line.attach_to(window)
+        window.render(1)
+        accum = accum + spacing
 
 
 # ----------------------------------------------------------------------
